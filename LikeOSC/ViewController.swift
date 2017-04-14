ba1a3bbcfdf1a10c6a3328fd8388da0ae063030a
Stephen Houser <stephenhouser@gmail.com>
2017-04-13 20:57:37 -0400

    Initial Commit

---
+++ LikeOSC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LikeOSC
+//
+//  Created by Stephen Houser on 4/13/17.
+//  Copyright © 2017 stephenhouser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
